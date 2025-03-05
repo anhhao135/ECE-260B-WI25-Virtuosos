f3f0868546707529d5862977ae174f2d633b38e9
Hao Le <51278143+anhhao135@users.noreply.github.com>
2025-03-04 21:49:04 -0800

    Update mac_col_tb.v

--- core/behavorial/mac_col/verilog/mac_col_tb.v
+++ core/behavorial/mac_col/verilog/mac_col_tb.v
@@ -2,10 +2,6 @@
 
 module mac_col_tb;
 
-reg [63:0] a = 0;
-reg [63:0] b = 0;
-wire [31:0] out;
-
 reg clk = 0;
 reg reset = 1;
 reg [63:0] q_in = 0;
@@ -21,6 +17,11 @@ wire [31:0] out_1;
 wire [63:0] q_out_1;
 wire [1:0] o_inst_1;
 wire fifo_wr_1;
+
+wire [31:0] out_2;
+wire [63:0] q_out_2;
+wire [1:0] o_inst_2;
+wire fifo_wr_2;
 
 
 mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(0)) mac_col_instance_0 (
@@ -45,6 +46,17 @@ mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(1)) mac_col_instance_1 (
       .fifo_wr(fifo_wr_1)
 );
 
+mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(1)) mac_col_instance_2 (
+      .clk(clk),
+      .reset(reset),
+      .out(out_2),
+      .q_in(q_out_1),
+      .q_out(q_out_2),
+      .i_inst(o_inst_1),
+      .o_inst(o_inst_2),
+      .fifo_wr(fifo_wr_2)
+);
+
 
 initial begin 
       $dumpfile("mac_col_tb.vcd");
@@ -58,7 +70,7 @@ initial begin
       #1 clk = 1;
       #1 clk = 0; q_in = {8'd1, 8'd3, 8'd5, 8'd7, 8'd9, 8'd11, 8'd13, 8'd15};
       #1 clk = 1;
-      #1 clk = 0; i_inst = 2'b00;
+      #1 clk = 0; q_in = {8'd2, 8'd4, 8'd6, 8'd8, 8'd10, 8'd12, 8'd14, 8'd16}; i_inst = 2'b00;
       #1 clk = 1;
       #1 clk = 0;
       #1 clk = 1;
