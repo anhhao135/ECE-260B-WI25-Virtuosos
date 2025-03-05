b58419f4424ccc0a19314a7cfb8d2e35c7180684
Hao Le <51278143+anhhao135@users.noreply.github.com>
2025-03-04 21:52:10 -0800

    Update mac_col_tb.v

--- core/behavorial/mac_col/verilog/mac_col_tb.v
+++ core/behavorial/mac_col/verilog/mac_col_tb.v
@@ -7,6 +7,7 @@ reg reset = 1;
 reg [63:0] q_in = 0;
 reg [1:0] i_inst = 0;
 
+integer i = 0;
 
 wire [31:0] out_0;
 wire [63:0] q_out_0;
@@ -46,7 +47,7 @@ mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(1)) mac_col_instance_1 (
       .fifo_wr(fifo_wr_1)
 );
 
-mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(1)) mac_col_instance_2 (
+mac_col #(.bw(8), .bw_psum(32), .pr(8), .col_id(2)) mac_col_instance_2 (
       .clk(clk),
       .reset(reset),
       .out(out_2),
@@ -71,10 +72,14 @@ initial begin
       #1 clk = 0; q_in = {8'd1, 8'd3, 8'd5, 8'd7, 8'd9, 8'd11, 8'd13, 8'd15};
       #1 clk = 1;
       #1 clk = 0; q_in = {8'd2, 8'd4, 8'd6, 8'd8, 8'd10, 8'd12, 8'd14, 8'd16}; i_inst = 2'b00;
-      #1 clk = 1;
-      #1 clk = 0;
-      #1 clk = 1;
-      #1 clk = 0;
+
+      for (i = 0; i < 10; i = i + 1) begin
+            #1 clk = 1;
+            #1 clk = 0;
+            #1 clk = 1;
+            #1 clk = 0;
+      end
+
       #1 $display("Finished.");
       #1 $finish;
 end
