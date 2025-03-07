ecd6e3d1c1d2d7354258590e4bccf650f22db3a8
Pethaperumal Natarajan <nk.petha@gmail.com>
2025-03-07 13:40:43 -0800

    done set up syn and pnr files

---
+++ core/pnr/netlist/core.v
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////
+// Created by: Synopsys DC Ultra(TM) in wire load mode
+// Version   : K-2015.06-SP2
+// Date      : Fri Mar  7 13:25:05 2025
+/////////////////////////////////////////////////////////////
+
+
+module core ( clk, sum_out, mem_in, out, inst, reset );
+  output [23:0] sum_out;
+  input [63:0] mem_in;
+  output [159:0] out;
+  input [16:0] inst;
+  input clk, reset;
+
+
+endmodule
+
