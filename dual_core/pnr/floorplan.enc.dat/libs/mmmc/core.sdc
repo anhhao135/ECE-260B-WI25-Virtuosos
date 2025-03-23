c3ee38cd33b888b59f5e70833caca52ebe746f6c
Hao Le <anhhao135@gmail.com>
2025-03-22 17:27:49 -0700

    added dual core rtl properly

--- dual_core/pnr/floorplan.enc.dat/libs/mmmc/core.sdc
+++
@@ -1,1 +0,0 @@
-/home/linux/ieng6/ee260bwi25/h3le/ECE-260B-WI25-Virtuosos/core/pnr/constraints/core.sdc
--- dual_core/pnr/setup_pnr.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ./netlist/*
-cp ../syn/core.out.v ./netlist/core.v
-
-rm -rf  ./constraints/*.sdc
-cp ../syn/core.sdc ./constraints/
